9b58e8ee8f185897e33ea55a6334d959cb964542
what-the-hub <iamthenight.yeah@gmail.com>
2022-01-13 14:54:32 +0200

    merge master

--- src/App.vue
+++ src/App.vue
@@ -11,25 +11,20 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #2c3e50
 
-#nav {
-  padding: 30px;
-  text-align: center;
+#nav
+  padding: 30px
+  text-align: center
+  a
+    font-weight: bold
+    color: #2c3e50
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+    &.router-link-exact-active
+      color: #42b983
 </style>
