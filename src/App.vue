cbda411929e141117946e4f14f41417425d5c87e
what-the-hub <iamthenight.yeah@gmail.com>
2022-02-11 12:57:49 +0200

    update button methods, header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/sign-up">Registration</router-link>
-      |
-      <router-link
-        to="/auth">Login
-      </router-link>
-      |
-      <router-link
-        to="/game">Game
-      </router-link>
+  <div>
+    <div id="app">
+      <div id="nav">
+        <router-link to="/sign-up">Registration</router-link>
+        <router-link to="/auth">Login</router-link>
+        <router-link to="/game">Game</router-link>
+      </div>
+      <div class="button-wrapper">
+        <button
+          v-if="this.$store.state.userStore.user"
+          @click="signOut"
+          class="btn btn-primary btn-sm"
+        >
+          Log Out
+        </button>
+        <button
+          v-if="!this.$store.state.userStore.user"
+          @click="redirectRoute"
+          class="btn btn-primary btn-sm"
+        >
+          Log In
+        </button>
+      </div>
     </div>
-    <button @click="signOut">logout</button>
     <router-view/>
   </div>
+
 </template>
 
 <script lang="ts">
@@ -24,8 +36,12 @@ import 'vue-class-component/hooks'
 
 @Component
 export default class App extends Vue {
-  signOut () {
+  signOut (): void {
     logOut()
+    this.redirectRoute()
+  }
+
+  redirectRoute (): void {
     if (this.$route.path !== '/auth') {
       this.$router.push('/auth')
     } else this.$router.push('/')
@@ -33,17 +49,23 @@ export default class App extends Vue {
 }
 </script>
 
-<style lang="sass">
+<style scoped lang="sass">
 #app
   font-family: Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
   color: #2c3e50
+  display: flex
+  justify-content: space-between
+  align-items: center
+
+.button-wrapper
+  padding: 10px
 
 #nav
-  padding: 30px
-  text-align: center
+  padding: 10px
   a
+    margin-right: 10px
     font-weight: bold
     color: #2c3e50
 
