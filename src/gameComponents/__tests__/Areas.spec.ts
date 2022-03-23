7aa75066e6261fe5f2101977412092b0544d6205
what-the-hub <iamthenight.yeah@gmail.com>
2022-03-23 16:59:18 +0200

    resolve comments #1

--- src/gameComponents/__tests__/Areas.spec.ts
+++ src/gameComponents/__tests__/Areas.spec.ts
@@ -1,6 +1,5 @@
 import { shallowMount, Wrapper } from '@vue/test-utils'
 import Areas from '@/gameComponents/Areas.vue'
-import Vue from 'vue'
 
 const baseMocks = {
   $store: {
@@ -39,62 +38,60 @@ describe('Areas.vue', () => {
   })
 
   it('emit should exist and return necessary obj', async () => {
-    vm.$emit('calculate-positions')
-    await Vue.nextTick()
-    expect(wrapper.emitted('calculate-positions')).toHaveLength(2)
+    // vm.$emit('calculate-positions')
+    // await Vue.nextTick()
+    // expect(wrapper.emitted('calculate-positions')).toHaveLength(1)
     expect(wrapper.emitted('calculate-positions')).toMatchObject([
       [{
         bottomExcellentArea: 0,
         bottomGoodArea: 0,
         topExcellentArea: 0,
         topGoodArea: 0
-      }],
-      []
+      }]
     ])
   })
-})
 
-describe('Areas.vue', () => {
-  beforeEach(() => {
-    wrapper = shallowMount(Areas, {
-      mocks: baseMocks
-    })
-    vm = wrapper.vm
-    wrapper.setData({
-      $store: {
-        state: {
-          scoreStore: {
-            score: 0,
-            lastStyle: 'good'
+  describe('Areas.vue styles', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(Areas, {
+        mocks: baseMocks
+      })
+      vm = wrapper.vm
+      wrapper.setData({
+        $store: {
+          state: {
+            scoreStore: {
+              score: 0,
+              lastStyle: 'good'
+            }
           }
         }
-      }
+      })
     })
-  })
 
-  it('should update necessary style', async () => {
-    await Vue.nextTick()
-    expect(vm.animationStyle).toMatchObject({
-      good: 'good-animation',
-      excellent: 'excellent-animation'
+    it('should update necessary style', () => {
+      expect(vm.animationStyle).toMatchObject({
+        good: 'good-animation',
+        excellent: 'excellent-animation'
+      })
     })
-  })
 
-  it('should find necessary style', function () {
-    expect(wrapper.find('#good').classes()).toContain('good-animation')
-  })
+    it('should find necessary style', function () {
+      expect(wrapper.find('#good').classes()).toContain('good-animation')
+    })
 
-  it('should not find necessary style', function () {
-    expect(wrapper.find('#excellent').classes()).not.toContain('good-animation')
-  })
+    it('should not find necessary style', function () {
+      expect(wrapper.find('#excellent').classes()).not.toContain('good-animation')
+    })
 
-  jest.useFakeTimers()
-  it('should reset style after timeout', function () {
-    jest.advanceTimersByTime(200)
+    jest.useFakeTimers()
+    it('should reset style after timeout', function () {
+      jest.advanceTimersByTime(200)
 
-    expect(vm.animationStyle).toEqual({
-      good: '',
-      excellent: ''
+      expect(vm.animationStyle).toEqual({
+        good: '',
+        excellent: ''
+      })
     })
   })
 })
